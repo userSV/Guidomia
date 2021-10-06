a693309877553707bce171f4f013d867ba9b9ab8
Shilpa Vashisht <vashishtshilpa001@gmail.com>
2021-10-06 09:24:50 +0530

    Listing screen Vehicle description header and footer view xibs added

---
+++ Guidomia/Application/Home/View/SeparatorView.swift
@@ -0,0 +1,15 @@
+//
+//  SeparatorView.swift
+//  Guidomia
+//
+//  Created by Shilpa on 06/10/21.
+//
+
+import UIKit
+
+/*
+ This view contains a line which acts as the separator for the table view.
+ */
+class SeparatorView: UITableViewHeaderFooterView {
+
+}
---
+++ Guidomia/Application/Home/View/VehicleDescriptionHeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  VehicleDescriptionHeaderView.swift
+//  Guidomia
+//
+//  Created by Shilpa on 05/10/21.
+//
+
+import UIKit
+
+class VehicleDescriptionHeaderView: UITableViewHeaderFooterView {
+
+    //MARK:- IBOutlets
+    @IBOutlet var vehicleImageView: UIImageView!
+    @IBOutlet var vehicleNameLabel: UILabel!
+    @IBOutlet var vehiclePriceLabel: UILabel!
+
+}
